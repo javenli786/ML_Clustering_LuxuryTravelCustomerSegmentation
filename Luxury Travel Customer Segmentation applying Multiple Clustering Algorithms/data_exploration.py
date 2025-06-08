ad800fdeb4fbac72cb1b5df7d2029dd9aa7486e6
Reazeng <jeff010426@gmail.com>
2025-06-08 15:48:27 +0800

    data_exploration.py

--- Luxury Travel Customer Segmentation applying Multiple Clustering Algorithms/data_exploration.py
+++ Luxury Travel Customer Segmentation applying Multiple Clustering Algorithms/data_exploration.py
@@ -13,6 +13,8 @@ def explore_data(df_guest_train, df_guest, df_booking):
     Counts_GWB = df_guest_train['GuestWithBooking'].value_counts(normalize=True).sort_values(ascending=False).mul(100).nlargest(2)
     print("Distribution of GuestWithBooking (%): \n", Counts_GWB)
 
+
+
     # Distribution of Country
     country_percentage = df_guest_train['Country'].value_counts(normalize=True).sort_values(ascending=False).mul(100).nlargest(5).round(3)
     print("Distribution of Country (%): \n", country_percentage)
@@ -21,6 +23,8 @@ def explore_data(df_guest_train, df_guest, df_booking):
     plt.title('Top 5 Country Percentage')
     plt.ylabel('Percentage')
     plt.show()
+
+
 
     # Distributio of BusinessUnitName
     BUN_percentage = df_guest_train['BusinessUnitName'].value_counts(normalize=True).sort_values(ascending=False).mul(100).round(3).nlargest(5)
@@ -31,6 +35,8 @@ def explore_data(df_guest_train, df_guest, df_booking):
     plt.ylabel('Percentage')
     plt.show()
 
+
+
     # Distribution of SegmentTitle
     ST_percentage = df_guest_train['SegmentTitle'].value_counts(normalize=True).sort_values(ascending=False).mul(100).round(3).nlargest(6)
     print("Distribution of SegmentTitle (%): \n", ST_percentage)
@@ -39,6 +45,8 @@ def explore_data(df_guest_train, df_guest, df_booking):
     plt.title('Top 6 SegmentTitle Percentage')
     plt.ylabel('Percentage')
     plt.show()
+
+
 
     # Distribution of LoyaltyClub
     LC_percentage = df_guest_train['LoyaltyClub'].value_counts(normalize=True).sort_values(ascending=False).mul(100).round(3).nlargest(6)
@@ -49,6 +57,8 @@ def explore_data(df_guest_train, df_guest, df_booking):
     plt.ylabel('Percentage')
     plt.show()
 
+
+
     # Distribution of RecordStatus
     RS_percentage = df_guest['RecordStatus'].value_counts(normalize=True).sort_values(ascending=False).mul(100).nlargest(8)
     print("Distribution of RecordStatus (%): \n", RS_percentage)
@@ -57,6 +67,8 @@ def explore_data(df_guest_train, df_guest, df_booking):
     plt.title('Top 8 RecordStatus Percentage')
     plt.ylabel('Percentage')
     plt.show()
+
+
 
     # 'df_booking'
 
@@ -67,6 +79,8 @@ def explore_data(df_guest_train, df_guest, df_booking):
     plt.title('Enquiry to Booking Conversion')
     plt.show()
 
+
+
     # Distribution of EnquiryDate
     plt.figure(figsize=(6, 6))
     sns.displot(df_booking['EnquiryDate'], kind='kde', color=sns.color_palette('deep')[0], fill=True)
@@ -74,6 +88,8 @@ def explore_data(df_guest_train, df_guest, df_booking):
     plt.xticks(rotation=90)
     plt.title('EnquiryDate Distribution')
     plt.show()
+
+
 
     # Distribution of MetaGroupName 
     MGN_percentage = df_booking['MetaGroupName'].value_counts(normalize=True).sort_values(ascending=False).mul(100).round(3).nlargest(10)
@@ -84,8 +100,12 @@ def explore_data(df_guest_train, df_guest, df_booking):
     plt.ylabel('Percentage')
     plt.show()
 
+
+
     # Descriptive statistics of Adults, Child, Infant, Nights
     print("Descriptive statistics of Adults, Child, Infant, Nights \n", df_booking[['Adults', 'Child', 'Infant', 'Nights']].describe().round(3))
+
+
 
     # Boxenplot of Adults, Child, Infant, Nights
     df_people_long = pd.melt(df_booking, value_vars=['Adults', 'Child', 'Infant'], var_name='Category', value_name='Count')
@@ -97,4 +117,44 @@ def explore_data(df_guest_train, df_guest, df_booking):
     axes[0].set_title('People Count')
     axes[1].set_title('Nights')
     plt.tight_layout()
+    plt.show()
+    
+    
+    
+    # RFM-related variables
+    df_guest_train_description = df_guest_train.drop(['ContactId', 'LatestDate', 'GuestWithBooking'], axis = 1).describe().round(2)
+    df_guest_train_description
+    
+    
+    # Boxenplot of EnquiryN, QuoteN, BookN by SegmentTitle
+    fig, axes = plt.subplots(1, 3, figsize = (12, 6))
+    notes = ['(a)', '(b)', '(c)']
+
+    i = 0
+    for n in ['EnquiryN', 'QuoteN', 'BookN']:
+        sns.boxenplot(data = df_guest_train, x = 'SegmentTitle', y = n, palette = 'deep', saturation = 0.8, ax = axes[i])
+        axes[i].grid(axis = 'y', linestyle = '-', alpha = 0.6)
+        axes[i].set_xticklabels(axes[i].get_xticklabels(), rotation = 90)
+        axes[i].set_yscale('log')
+        axes[i].text(-0.1, 1.05, notes[i], transform = axes[i].transAxes, fontsize = 12, fontweight = 'bold', va = 'top', ha = 'right')
+        i += 1
+
+    plt.tight_layout()
+    plt.show()
+    
+    
+    
+    #Boxenplot of Days by SegmentTitle
+    plt.grid(axis = 'y', linestyle = '-', alpha = 0.3)
+    sns.boxenplot(data = df_guest_train, y = 'Days', x = 'SegmentTitle', palette = 'deep', saturation = 0.8)
+    plt.xticks(rotation = 90)
+    plt.show()
+    
+    
+    
+    # Boxenplot of CommercialValueGBP by SegmentTitle
+    plt.grid(axis = 'y', linestyle = '-', alpha = 0.3)
+    sns.boxenplot(data = df_guest_train, y = 'CommercialValueGBP', x = 'SegmentTitle', palette = 'deep', saturation = 0.8)
+    plt.xticks(rotation = 90)
+    plt.yscale('symlog')
     plt.show()
